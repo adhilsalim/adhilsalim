502fbe9514c22d35d02b1b20b2a464882485c4ad
Adhil Salim <bizadhil@gmail.com>
2023-12-13 20:54:54 +0530

    add test comments

--- generate_banner.py
+++ generate_banner.py
@@ -2,6 +2,11 @@ import sys
 from PIL import Image
 
 def create_image_with_heart(x, y):
+
+    # TEST
+    print(f"X-coordinate input from user: {x}")
+    print(f"Y-coordinate input from user: {y}")
+
     width, height = 1250, 650  
     background_color = (0, 0, 0)  
     img = Image.new("RGBA", (width, height), background_color)
@@ -11,24 +16,24 @@ def create_image_with_heart(x, y):
     heart_image.thumbnail((50, 50), Image.LANCZOS)
 
     if 0 <= x <= 600:
-        x_position = x
+        x = x
     elif -600 <= x < 0:
-        x_position = 600 + x
+        x = 600 + x
     else:
-        x_position = 600
+        x = 600
 
     if 0 <= y <= 300:
-        y_position = 300 - y
+        y = 300 - y
     elif -300 <= y < 0:
-        y_position = 300 + y
+        y = 300 + y
     else:
-        y_position = 300
+        y = 300
 
-    print(f"x axis of heart: {x_position}")
-    print(f"y axis of heart: {y_position}")
+    # TEST
+    print(f"x axis of heart: {x}")
+    print(f"y axis of heart: {y}")
 
-    img.paste(heart_image, (x_position, y_position), heart_image)
-
+    img.paste(heart_image, (x, y), heart_image)
     img.save("github_banner_heart.png")
 
 if __name__ == "__main__":
@@ -39,5 +44,11 @@ if __name__ == "__main__":
     x_input = int(sys.argv[1])
     y_input = int(sys.argv[2])
 
+    # TEST
+    print(f"X-coordinate input from user: {x_input}")
+    print(f"Y-coordinate input from user: {y_input}")
+
     create_image_with_heart(x_input, y_input)
+
+    # TEST
     print("Image created and saved as github_banner_heart.png")
