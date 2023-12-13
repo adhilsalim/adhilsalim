dd19797c6e708f8984f1312d7cc74e7e386d9934
Adhil Salim <69722917+adhilsalim@users.noreply.github.com>
2023-12-13 15:33:45 +0000

    bug fix: loc mismatch

--- generate_banner.py
+++ generate_banner.py
@@ -3,39 +3,36 @@ from PIL import Image
 
 def create_image_with_heart(x, y):
 
-    # TEST
-    print(f"X-coordinate input from user: {x}")
-    print(f"Y-coordinate input from user: {y}")
-
     width, height = 1250, 650  
-    background_color = (0, 0, 0)  
+    background_color = (0, 0, 0)
     img = Image.new("RGBA", (width, height), background_color)
-
     heart_image = Image.open("images/heart.png")
     heart_image = heart_image.convert("RGBA")
     heart_image.thumbnail((50, 50), Image.LANCZOS)
 
-    if 0 <= x <= 600:
-        x = x
-    elif -600 <= x < 0:
-        x = 600 + x
+    r, g, b, a = heart_image.split()
+    mask = a
+
+    if (x >= 0 and x <= 600):
+        x = x+600
+    elif (x < 0 and x>=-600):
+        x = (600-(x*-1))
     else:
         x = 600
-
-    if 0 <= y <= 300:
-        y = 300 - y
-    elif -300 <= y < 0:
-        y = 300 + y
+    
+    if (y >= 0 and y <= 300):
+        y = 300-y
+    elif (y < 0 and y>=-300):
+        y = (y*-1)+300
     else:
         y = 300
 
-    # TEST
+
     print(f"x axis of heart: {x}")
     print(f"y axis of heart: {y}")
 
-    img.paste(heart_image, (x, y), heart_image)
+    img.paste(heart_image, (x, y), mask)
     img.save("github_banner_heart.png")
-
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python generate_banner.py <X-coordinate> <Y-coordinate>")
